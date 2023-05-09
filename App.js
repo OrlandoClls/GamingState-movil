ede0cfe9534f0ccf660b18aecc7621ff6cbd655d
Orlando Callejas <60374663+OrlandoClls@users.noreply.github.com>
2023-05-09 13:44:37 -0700

    Api

--- App.js
+++ App.js
@@ -1,20 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import React, {useState, useEffect} from "react";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>Hola</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
+const ApiRawg = () => {
+  const url = "https://api.rawg.io/api/games";
+  const key = "7320353049404ffdaab57f1cba2972a4"
+  const [games, setGames] = useState([]);
+  const [loading, setLoading] = useState(true);
+  fetch(`${url}?key=${key}`)
+    .then((response) => response.json())
+    .then((data) => {
+      setGames(data.results);
+      setLoading(false);
+    })
+    .catch((error) => console.log(error));
+  return { games, loading };
+};
+
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#292929",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: "bold",
+    color: "#fff",
   },
 });
